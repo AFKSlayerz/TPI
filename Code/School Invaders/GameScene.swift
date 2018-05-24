a8d2c6a3ad566979ab26dec70129f9724036a131
Mikel <mikel.harnisch@gmail.com>
2018-05-24 16:26:16 +0200

    Code Update
    Update GameScene:
    - Affiche les mots qui correspondent au vocs
    - Eleve se déplace de gauche à droite
    - Prof se déplace de gauche a droit et s'arrete selon ou l'on touche sur l'écran

--- Code/School Invaders/GameScene.swift
+++ Code/School Invaders/GameScene.swift
@@ -20,8 +20,34 @@ class GameScene: SKScene {
     let Student = SKSpriteNode(imageNamed: "Student.png")
     let Teacher = SKSpriteNode(imageNamed: "Teacher.png")
     let PaperPlane = SKSpriteNode(imageNamed: "PaperPlane.png")
+    let MoveRight = SKSpriteNode()
+    let MoveLeft = SKSpriteNode()
+    let StopMove = SKSpriteNode()
+    var Box = SKSpriteNode(imageNamed: "WordCase")
+    var BoxX = SKSpriteNode()
+    let Box1 = SKSpriteNode(imageNamed: "WordCase")
+    let Box2 = SKSpriteNode(imageNamed: "WordCase")
+    let Box3 = SKSpriteNode(imageNamed: "WordCase")
+    let Box4 = SKSpriteNode(imageNamed: "WordCase")
     let GameStared:Bool = false
-
+    var VocWordTxT = SKLabelNode()
+    var VocWordStudent = SKLabelNode()
+    var MyVocWordStudent = [SKLabelNode]()
+    var Test = SKLabelNode()
+    var i:Int = 0
+    var VocX:Int = 110
+    var VocY:Int = 30
+    var WordX:Int = 85
+    var WordY:Int = 50
+    var TeacherX:Int = 0
+    var TeacherY:Int = 0
+    var PaperPlaneX:Int = 0
+    var PaperPlaneY:Int = 0
+    var WordCase = SKSpriteNode(imageNamed: "WordCase.png")
+    var VocWordStudentCase = SKSpriteNode()
+    var TeacherSelected:String = ""
+   
+    
     // Screen width.
     public var screenWidth: CGFloat {
         return UIScreen.main.bounds.width
@@ -31,20 +57,31 @@ class GameScene: SKScene {
     public var screenHeight: CGFloat {
         return UIScreen.main.bounds.height
     }
+    
     override init(size: CGSize) {
         super.init(size: size)
         
+        TeacherX = Int(screenWidth) / 2
+        TeacherY = Int(screenHeight) / 5
+        PaperPlaneX = Int(screenWidth) / 2
+        PaperPlaneY = Int(screenHeight) / 3
+        let moveLeft = SKAction.moveBy(x: 900, y: 0, duration: 10)
+        let sequence = SKAction.sequence([moveLeft, moveLeft.reversed()])
+        
         backgroundColor = SKColor.white
         
-        Student.position = CGPoint(x: size.width / 2, y: size.height / 1.1)  //The game will be built from the top-left
+        Student.position = CGPoint(x: 50, y: size.height / 1.1)  //The game will be built from the top-left
         Student.size = CGSize(width: 80, height: 90)  //The anchor point (top left)
         addChild(Student)
         
-        Teacher.position = CGPoint(x: size.width / 2, y: size.height / 7)  //The game will be built from the top-left
+        
+        Student.run(SKAction.repeatForever(sequence), withKey:  "moving")
+
+        Teacher.position = CGPoint(x: TeacherX, y: TeacherY)  //The game will be built from the top-left
         Teacher.size = CGSize(width: 80, height: 90)  //The anchor point (top left)
         addChild(Teacher)
         
-        PaperPlane.position = CGPoint(x: size.width / 2, y: size.height / 4)  //The game will be built from the top-left
+        PaperPlane.position = CGPoint(x: PaperPlaneX, y: PaperPlaneY)  //The game will be built from the top-left
         PaperPlane.size = CGSize(width: 50, height: 80)  //The anchor point (top left)
         addChild(PaperPlane)
         
@@ -55,47 +92,126 @@ class GameScene: SKScene {
         Back.position = CGPoint(x: size.width / 1.04, y: size.height / 1.035)
         addChild(Back)
         
-        switch flag{
-        case 3: Voc1Eng.fontSize = 30
-        Voc1Eng.fontColor = SKColor.black
-        Voc1Eng.text = "Voc1 anglais"
-        Voc1Eng.position = CGPoint(x: size.width / 7, y: size.height / 1.75)
-        addChild(Voc1Eng)
-            
-        case 4: Voc2Eng.fontSize = 30
-        Voc2Eng.fontColor = SKColor.black
-        Voc2Eng.text = "Voc2 anglais"
-        Voc2Eng.position = CGPoint(x: size.width / 7, y: size.height / 1.75)
-        addChild(Voc2Eng)
-            
-        case 13:Voc3Eng.fontSize = 30
-        Voc3Eng.fontColor = SKColor.black
-        Voc3Eng.text = "Voc3 français"
-        Voc3Eng.position = CGPoint(x: size.width / 7, y: size.height / 1.75)
-        addChild(Voc3Eng)
-            
-        case 14:Voc4Eng.fontSize = 30
-        Voc4Eng.fontColor = SKColor.black
-        Voc4Eng.text = "Voc4 français"
-        Voc4Eng.position = CGPoint(x: size.width / 7, y: size.height / 1.75)
-        addChild(Voc4Eng)
-            
-        default:Error.fontSize = 50
-        Error.fontColor = SKColor.black
-        Error.text = "Error"
-        Error.position = CGPoint(x: size.width / 2, y: size.height / 2)
-        addChild(Error)
+        Box1.position = CGPoint(x: size.width / 9, y: 50)
+        Box1.size = CGSize(width: 200, height: 85)
+        Box1.name = "Box1"; // set the name for your sprite
+        Box1.isUserInteractionEnabled = false; // userInteractionEnabled should be disabled
+        self.addChild(Box1)
+        Box2.position = CGPoint(x: size.width / 2.7, y: 50)
+        Box2.size = CGSize(width: 200, height: 85)
+        Box2.name = "Box2"; // set the name for your sprite
+        Box2.isUserInteractionEnabled = false; // userInteractionEnabled should be disabled
+        self.addChild(Box2)
+        Box3.position = CGPoint(x: size.width / 1.6, y: 50)
+        Box3.size = CGSize(width: 200, height: 85)
+        Box3.name = "Box3"; // set the name for your sprite
+        Box3.isUserInteractionEnabled = false; // userInteractionEnabled should be disabled
+        self.addChild(Box3)
+        Box4.position = CGPoint(x: size.width / 1.12, y: 50)
+        Box4.size = CGSize(width: 200, height: 85)
+        Box4.name = "Box4"; // set the name for your sprite
+        Box4.isUserInteractionEnabled = false; // userInteractionEnabled should be disabled
+        self.addChild(Box4)
+        
+        MoveRight.color = SKColor.green
+        MoveRight.position = CGPoint(x: size.width / 0.9, y: size.height/5)
+        MoveRight.size = CGSize(width: size.width/1.4, height: size.height/8)
+        MoveRight.name = "MoveRight"; // set the name for your sprite
+        MoveRight.isUserInteractionEnabled = false; // userInteractionEnabled should be disabled
+        self.addChild(MoveRight)
+        
+        MoveLeft.color = SKColor.blue
+        MoveLeft.position = CGPoint(x: size.width / 8 , y: size.height/5)
+        MoveLeft.size = CGSize(width: size.width / 4.1, height: size.height/8)
+        MoveLeft.name = "MoveLeft"; // set the name for your sprite
+        MoveLeft.isUserInteractionEnabled = false; // userInteractionEnabled should be disabled
+        self.addChild(MoveLeft)
+        
+        StopMove.color = SKColor.gray
+        StopMove.position = CGPoint(x: size.width / 2 , y: size.height/5)
+        StopMove.size = CGSize(width: size.width / 2, height: size.height/8)
+        StopMove.name = "StopMove"; // set the name for your sprite
+        StopMove.isUserInteractionEnabled = false; // userInteractionEnabled should be disabled
+        self.addChild(StopMove)
+        
+        /*for _ in 0...5
+        {
+            VocWordStudentCase = WordCase.copy() as! SKSpriteNode
+            VocWordStudentCase.size = CGSize(width: 150, height: 75)
+            VocWordStudentCase.position = CGPoint(x: WordX , y: WordY)
+            addChild(VocWordStudentCase)
+            WordX += 170
+        }*/
+        for vocid in VocProvider().VocWords[3]{
+            if ChosenVoc == Int(vocid)
+            {
+                var line = ""
+                VocWordStudent = VocWordTxT.copy() as! SKLabelNode
+                line += String(VocProvider().VocWords[1][i])
+                line += " "
+                VocWordStudent.zPosition = 1
+                VocWordStudent.fontName = "Arial-Bold"
+                VocWordStudent.fontSize = 50
+                VocWordStudent.fontColor = SKColor.black
+                VocWordStudent.text = line
+                VocWordStudent.position = CGPoint(x: VocX , y: VocY)
+                MyVocWordStudent.append(VocWordStudent)
+                addChild(VocWordStudent)
+                VocX = VocX + 270
+            }
+            i+=1
         }
+        
     }
 
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in (touches) {
+            let positionInScene = touch.location(in: self)
+            let touchedNode = self.atPoint(positionInScene)
+            if let name = touchedNode.name {
+                
+                if name == "MoveLeft" {
+                    while TeacherX > 90{
+                    TeacherX = TeacherX - 5
+                    let moveAction: SKAction = SKAction.moveBy(x: -5, y: 0, duration: 15)
+                    Teacher.run(moveAction)
+                    PaperPlane.run(moveAction)}
+                }
+                
+                if name == "MoveRight" {
+                    while TeacherX < 920{
+                    TeacherX += 5
+                    let moveAction: SKAction = SKAction.moveBy(x: 5, y: 0, duration: 15)
+                    Teacher.run(moveAction)
+                    PaperPlane.run(moveAction)
+                    }
+                }
+                
+                if name == "StopMove" {
+                    Teacher.removeAllActions()
+                    PaperPlane.removeAllActions()
+                }
+            }
+        }
+    }
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first
         let touchLocation = touch!.location(in: self)
-        
+
+        for word in MyVocWordStudent {
+            if word.contains(touchLocation){
+                TeacherSelected = word.text!
+                Test.fontSize = 24
+                Test.fontColor = SKColor.black
+                Test.fontName = "Arial-Bold"
+                Test.text = TeacherSelected
+                Test.position = CGPoint(x: size.width / 2, y: size.height / 2)
+                addChild(Test)
+            }
+        }
         if Back.contains(touchLocation) {
             flag = 0
             let reveal = SKTransition.doorsOpenVertical(withDuration: 0.5)
--- Code/School Invaders/MenuScene.swift
+++ Code/School Invaders/MenuScene.swift
@@ -10,102 +10,129 @@ import UIKit
 import SpriteKit
 
 public var flag: Int = 0
+public var ChosenVoc:Int = 0
 
 class MenuScene: SKScene {
     let SelectLanguageStudent = SKLabelNode()
     let SelectLanguageProf = SKLabelNode()
-    let EnglishVoc = SKLabelNode()
-    let FrancaisVoc = SKLabelNode()
+    let Test1 = SKLabelNode()
+    let Test2 = SKLabelNode()
     var Voc = SKLabelNode()
     var LanguagesTxT = SKLabelNode()
     var VocTxT = SKLabelNode()
+    let ChooseVoc = SKLabelNode()
     let Back = SKLabelNode()
     let GO = SKLabelNode()
     let button = UIButton(type: UIButtonType.system) as UIButton
     var x:Int = 0
-    var y:Int = 0
-    var yId:Int = 0
-    var b:Int = 0
-    var z:Int = 0
     var Id:Int = 0
-    var w:Int = 0
     var i:Int = 0
     var flag:Int = 0
-    var LanguagesX:Int = 200
-    var LanguagesY:Int = 400
-    var VocX:Int = 500
-    var VocY:Int = 400
+    var LanguagesX:Int = 0
+    var LanguagesY:Int = 0
+    var VocX:Double = 0
+    var VocY:Int = 0
     var MyWords = [SKLabelNode]()
     var MyVocs = [SKLabelNode]()
-    var Word = SKLabelNode()
     var label = SKLabelNode()
     var LanguageProf:Int = 0
     var LanguageEleve:Int = 0
     var LabelId:String = "-1"
-    var VocId:String = "1"
+    var VocId:String = "0"
+    
+    public var screenWidth: CGFloat {
+        return UIScreen.main.bounds.width
+    }
+    
+    // Screen height.
+    public var screenHeight: CGFloat {
+        return UIScreen.main.bounds.height
+    }
     override init(size: CGSize) {
         super.init(size: size)
-        
+        LanguagesX = Int(screenWidth) / 5
+        LanguagesY = Int(screenHeight) / 2
+        VocX = Double(screenWidth) / 1.4
+        VocY = Int(screenHeight) / 2
         backgroundColor = SKColor.white
         
         
         
         //Select language of the professor
-        SelectLanguageProf.fontSize = 30
+        SelectLanguageProf.fontSize = 48
         SelectLanguageProf.fontColor = SKColor.black
         SelectLanguageProf.fontName = "Arial-Bold"
-        SelectLanguageProf.text = "Select the professor language"
+        SelectLanguageProf.text = "Teacher language"
         SelectLanguageProf.position = CGPoint(x: size.width / 4.5, y: size.height / 1.75)
         addChild(SelectLanguageProf)
         
         
-        for word in VocProvider().Languages[1]{
+        for _ in VocProvider().Languages[1]{
             var line = ""
             label = LanguagesTxT.copy() as! SKLabelNode
             line += String(VocProvider().Languages[1][x])
             line += " "
             label.fontName = "Arial-Bold"
-            label.fontSize = 28
+            label.fontSize = 40
             label.text = line
             label.fontColor = SKColor.black
             label.position = CGPoint(x: LanguagesX , y: LanguagesY)
-            y = Int(LabelId)!
-            self.y+=1
-            LabelId = String(y)
+            var z = Int(LabelId)!
+            z+=1
+            LabelId = String(z)
             label.name = LabelId
             MyWords.append(label)
             addChild(label)
-            LanguagesY = LanguagesY - 50
+            LanguagesY = LanguagesY - 80
             x+=1
         }
         //Select language of the student
-        SelectLanguageStudent.fontSize = 30
+        SelectLanguageStudent.fontSize = 48
         SelectLanguageStudent.fontColor = SKColor.black
         SelectLanguageStudent.fontName = "Arial-Bold"
-        SelectLanguageStudent.text = "Select the student language"
+        SelectLanguageStudent.text = "Student language"
         SelectLanguageStudent.position = CGPoint(x: size.width / 4.5, y: size.height / 1.75)
         addChild(SelectLanguageStudent)
         SelectLanguageStudent.isHidden = true
-
-        EnglishVoc.fontSize = 40
-        EnglishVoc.fontColor = SKColor.black
-        EnglishVoc.fontName = "Arial-Bold"
-        EnglishVoc.text = "English Voc"
-        EnglishVoc.position = CGPoint(x: size.width / 1.05, y: size.height / 1.75)
-        addChild(EnglishVoc)
-        EnglishVoc.isHidden = true
+        /*
+        Test1.fontSize = 40
+        Test1.fontColor = SKColor.black
+        Test1.fontName = "Arial-Bold"
+        Test1.text = "English Voc"
+        Test1.position = CGPoint(x: size.width / 1.05, y: size.height / 1.75)
+        addChild(Test1)
+        Test1.isHidden = true*/
+        
+        /*
+        Test2.fontSize = 40
+        Test2.fontColor = SKColor.black
+        Test2.fontName = "Arial-Bold"
+        Test2.text = "Francais Voc"
+        Test2.position = CGPoint(x: size.width / 1.05, y: size.height / 2.1)
+        addChild(Test2)
+        Test2.isHidden = true
+        */
         
         
-        FrancaisVoc.fontSize = 40
-        FrancaisVoc.fontColor = SKColor.black
-        FrancaisVoc.fontName = "Arial-Bold"
-        FrancaisVoc.text = "Francais Voc"
-        FrancaisVoc.position = CGPoint(x: size.width / 1.05, y: size.height / 2.1)
-        addChild(FrancaisVoc)
-        FrancaisVoc.isHidden = true
+         Test2.fontSize = 40
+         Test2.fontColor = SKColor.black
+         Test2.fontName = "Arial-Bold"
+         Test2.text = "Francais Voc"
+         Test2.position = CGPoint(x: size.width / 1.05, y: size.height / 2.1)
+         addChild(Test2)
+         Test2.isHidden = true
+        
+        
+        ChooseVoc.fontSize = 45
+        ChooseVoc.fontColor = SKColor.black
+        ChooseVoc.fontName = "Arial-Bold"
+        ChooseVoc.text = "Choose a vocabulary"
+        ChooseVoc.position = CGPoint(x: size.width / 1.35, y: size.height / 1.75)
+        addChild(ChooseVoc)
+        ChooseVoc.isHidden = true
         
         //GO
-        GO.fontSize = 40
+        GO.fontSize = 50
         GO.fontColor = SKColor.black
         GO.fontName = "Arial-Bold"
         GO.text = "GO !"
@@ -116,7 +143,8 @@ class MenuScene: SKScene {
         if flag == 0{
             Back.isHidden = true
         }
-        Back.fontSize = 28
+        
+        Back.fontSize = 34
         Back.fontColor = SKColor.black
         Back.fontName = "Arial-Bold"
         Back.text = "Retour"
@@ -132,40 +160,45 @@ class MenuScene: SKScene {
         let touch = touches.first
         let touchLocation = touch!.location(in: self)
         
+        //If the student language is chosen
         if flag == 1{
-            flag += 1
+            flag = 2
             for word in MyWords {
                 if word.contains(touchLocation){
                     SelectLanguageStudent.isHidden = false
                     SelectLanguageProf.isHidden = true
                     Back.isHidden = false
-                    z = Int(word.name!)!
+                    let z = Int(word.name!)!
                     LanguageEleve = Int(VocProvider().Languages[0][z])!
-                    FrancaisVoc.isHidden = false
-                    FrancaisVoc.text = String(LanguageEleve)
+                    //Test2.isHidden = false
+                    //Test2.text = String(LanguageEleve)
+                    ChooseVoc.isHidden = false
                 }
             }
         }
         
+        //If the professor language is chosen
         if flag == 0{
-            flag += 1
+            flag = 1
             for word in MyWords {
                 if word.contains(touchLocation){
                     SelectLanguageStudent.isHidden = false
                     SelectLanguageProf.isHidden = true
                     Back.isHidden = false
-                    z = Int(word.name!)!
+                    let z = Int(word.name!)!
                     LanguageProf = Int(VocProvider().Languages[0][z])!
-                    EnglishVoc.isHidden = false
-                    EnglishVoc.text = String(LanguageProf)
+                    //Test1.isHidden = false
+                    //Test1.text = String(LanguageProf)
                 }
             }
         }
-        while flag == 2{
-            for ID in VocProvider().Vocabulary[0]
+        
+        //if the professor and student language are chosen
+        if flag == 2{
+            for _ in VocProvider().Vocabulary[0]
             {
-                var lngprof = VocProvider().Vocabulary[2][i]
-                var lngeleve = VocProvider().Vocabulary[3][i]
+                let lngprof = VocProvider().Vocabulary[2][i]
+                let lngeleve = VocProvider().Vocabulary[3][i]
                 if lngprof == String(LanguageProf) && lngeleve == String(LanguageEleve)
                 {
                     var line = ""
@@ -173,110 +206,54 @@ class MenuScene: SKScene {
                     line += String(VocProvider().Vocabulary[1][i])
                     line += " "
                     Voc.fontName = "Arial-Bold"
-                    Voc.fontSize = 28
+                    Voc.fontSize = 40
                     Voc.fontColor = SKColor.black
                     Voc.text = line
-                    Voc.position = CGPoint(x: VocX , y: VocY)
-                    yId = Int(LabelId)!
-                    self.yId+=1
-                    LabelId = String(yId)
-                    Voc.name = LabelId
+                    Voc.position = CGPoint(x: Int(VocX) , y: VocY)
+                    VocId = String(i)
+                    Voc.name = VocId
                     MyVocs.append(Voc)
                     addChild(Voc)
-                    VocY = VocY - 50
+                    VocY = VocY - 80
                 }
                 i+=1
             }
-            flag += 1
+            flag = 3
         }
+        
         if flag == 1{
             SelectLanguageStudent.isHidden = false
             SelectLanguageProf.isHidden = true
         }
+        
+        //If a vocabulary is chosen
+        if flag == 3{
+            for Vocs in MyVocs {
+                if Vocs.contains(touchLocation){
+                    GO.isHidden = false
+                    let z = Int(Vocs.name!)!
+                    ChosenVoc = Int(VocProvider().Vocabulary[0][z])!
+                    flag = 4
+                }
+            }
+        }
+        
+        if GO.contains(touchLocation)
+        {
+            let reveal = SKTransition.doorsOpenVertical(withDuration: 0.5)
+            let gameScene = GameScene(size: self.size)
+            self.view?.presentScene(gameScene, transition: reveal)
+        }
+        
         /*if Back.contains(touchLocation) && flag >= 1{
-                Id = Id - 1
-
+            flag-=1
                 switch flag{
-                    
-                case 0: LanguagesTxT.fontColor = SKColor.black
-                        SelectLanguageProf.isHidden = false
-                        SelectLanguageStudent.isHidden = true
-                        Back.isHidden = true
-                    
-                case 1: LanguagesTxT.fontColor = SKColor.black
-                        EnglishVoc.isHidden = true
-                        Voc1.isHidden = true
-                        Voc2.isHidden = true
-
-                    
-                case 2: Voc1.fontColor = SKColor.black
-                        GO.isHidden = true
-                    
-                case 3: Voc2.fontColor = SKColor.black
-                        GO.isHidden = true
-                        flag = flag - 1
-                    
-                case 10:LanguagesTxT.fontColor = SKColor.black
-                        SelectLanguageProf.isHidden = false
-                        SelectLanguageStudent.isHidden = true
-                        Back.isHidden = true
-                        flag = flag - 10
-                    
-                case 11:LanguagesTxT.fontColor = SKColor.black
-                        FrancaisVoc.isHidden = true
-                        Voc3.isHidden = true
-                        Voc4.isHidden = true
-                    
-                case 12:Voc3.fontColor = SKColor.black
-                        GO.isHidden = true
-                    
-                case 13:Voc4.fontColor = SKColor.black
-                        GO.isHidden = true
-                        flag = flag - 1
-                    
+            
+                case 2: GO.isHidden = true
                 
                 default:
                     flag = flag + 0
                 }
-        }
-        if LanguagesTxT.contains(touchLocation)  && flag == 0 {
-            SelectLanguageStudent.isHidden = false
-            SelectLanguageProf.isHidden = true
-            flag = 11
-            Back.isHidden = false
-            LanguagesTxT.fontColor = SKColor.gray
-
-        }else if LanguagesTxT.contains(touchLocation)  && flag == 11 {
-            FrancaisVoc.isHidden = false
-            Voc3.isHidden = false
-            Voc4.isHidden = false
-            flag = 12
-            LanguagesTxT.fontColor = SKColor.gray
-
-        }else if Voc1.contains(touchLocation) && flag == 2{
-            flag = 3
-            GO.isHidden = false
-            Voc1.fontColor = SKColor.gray
-
-        }else if Voc2.contains(touchLocation) && flag == 2{
-            flag = 4
-            GO.isHidden = false
-            Voc2.fontColor = SKColor.gray
-
-        }else if Voc3.contains(touchLocation) && flag == 12{
-            flag = 13
-            GO.isHidden = false
-            Voc3.fontColor = SKColor.gray
-
-        }else if Voc4.contains(touchLocation) && flag == 12{
-            flag = 14
-            GO.isHidden = false
-            Voc4.fontColor = SKColor.gray
-
-        }else if GO.contains(touchLocation){
-            let reveal = SKTransition.doorsOpenVertical(withDuration: 0.5)
-            let gameScene = GameScene(size: self.size)
-            self.view?.presentScene(gameScene, transition: reveal)
         }*/
     }
 }
