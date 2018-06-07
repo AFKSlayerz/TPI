ed352d8dc8a3bce256147c7a37c113c37ea421e8
Mikel <mikel.harnisch@gmail.com>
2018-06-07 08:32:04 +0200

    Final update

--- Code/School Invaders/VocProvider.swift
+++ Code/School Invaders/VocProvider.swift
@@ -12,6 +12,6 @@ import SpriteKit
 class VocProvider {
     let Languages = [["1","2","3","4"], ["français","anglais","allemand","Espagnol"]]
     let Vocabulary = [["1","2","3","4","5","6","7","8"], ["Les Couleurs","Le Restaurant","Les Affaires","L'art","Les Couleurs","Le Restaurant","Les Affaires","Le Restaurant"], ["1","1","1","1","1","1","2","2"], ["2","2","2","2","3","3","3","4"]]
-    let VocWords = [["1264","1265","1283","1284","1313","1314","1366","1367","1431","1432","1404","1406","1452","1453","1497","1498"], ["blanc","bleu","le garçon","la serveuse","le client","le contract","le tableau","la toile","gris","rouge","la carte","le pourboire","boss","meeting","the waiter","the waitress"], ["white","blue","the waiter","the waitress","client","business deal","painting","canvas","grau","rot","das Menu","das Tipp","der Chef","das Treffen","el camarero","el camarera"], ["1","1","2","2","3","3","4","4","5","5","6","6","7","7","8","8"]]
+    let VocWords = [["1264","1265","1266","1267","1283","1284","1313","1314","1366","1367","1431","1432","1404","1406","1452","1453","1497","1498"], ["blanc","bleu","rouge","vert","le garçon","la serveuse","le client","le contract","le tableau","la toile","gris","rouge","la carte","le pourboire","boss","meeting","the waiter","the waitress"], ["white","blue","red","green","the waiter","the waitress","client","business deal","painting","canvas","grau","rot","das Menu","das Tipp","der Chef","das Treffen","el camarero","el camarera"], ["1","1","1","1","2","2","3","3","4","4","5","5","6","6","7","7","8","8"]]
 }
 
